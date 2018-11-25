ab673442173697dd5604a11acc6dacdc1912d27b
Peter Nagy <42371686+NagyDRCS@users.noreply.github.com>
2018-11-25 20:44:52 +0100

    footer
    zmena vzhladu footer

--- footer.php
+++ footer.php
@@ -13,20 +13,20 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'luna' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'luna' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'luna' ), 'luna', '<a href="http://drossel.sk">Drossel | Creative Studio</a>' );
-				?>
-		</div><!-- .site-info -->
+	<footer id="colophon">
+		<div class="colophone-one">
+			<div class="colophone-two"></div>
+		</div>
+		<div class="site-footer">
+			<div class="wrapp">
+				<div class="site-footer-nesletter">
+					NEWSLETTER
+				</div>
+				<div class="site-footer-menu">
+					FOOTER MENU
+				</div>
+			</div>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
