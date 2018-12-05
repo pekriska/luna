3d1a4bb2aa96c2bbfd610f2a7b7d17f75c024021
Peter Nagy <42371686+NagyDRCS@users.noreply.github.com>
2018-12-05 17:56:20 +0100

    123
    123

--- inc/templates/contact-form.php
+++ inc/templates/contact-form.php
@@ -1,28 +1,36 @@
 <?php
 // kostra kontaktneho formularu.
 ?>
-<form id="drosselContactForm" action="#" method="post" data-url="<?php echo admin_url('admin-ajax.php'); ?>">
+<form id="drosselContactForm"  action="#" method="post" data-url="<?php echo admin_url('admin-ajax.php'); ?>">
 
-	<div class="form-group">
-                <h4>Meno a priezvisko*</h4>
-                <input type="text" class="form-control" placeholder="Vaše meno*" id="name" name="name">
-                <small class="text-danger form-control-msg">Zadajte meno</small>
-	</div>
+	<div class="site-contact-left">
+        <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Vaše meno*" id="name" name="name">
+                    <small class="text-danger form-control-msg">Zadajte meno</small>
+        </div>
 
-	<div class="form-group">
-                <h4>E mail*</h4>
-                <input type="text" class="form-control" placeholder="Váš email*" id="email" name="email" >
-                <small class="text-danger form-control-msg">Zadajte správny e-mail</small>
-	</div>
-
-	<div class="form-group">
-                <h4>Vaša správa*</h4>
-                <textarea name="message" id="message" class="form-control" placeholder="Vaša správa"></textarea>
-                <small class="text-danger form-control-msg">Zadajte správu</small>
-	</div>
+        <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Váš email*" id="email" name="email" >
+                    <small class="text-danger form-control-msg">Zadajte správny e-mail</small>
+        </div>
+        <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Kontaktné číslo" id="phone" name="phone" >
+        </div>
+    </div>
+    <div class="site-contact-left">
+        <div class="form-group">
+                    <textarea name="message" id="message" class="form-control" placeholder="Vaša správa*"></textarea>
+                    <small class="text-danger form-control-msg">Zadajte správu</small>
+        </div>
+    </div>
+    <div class="site-contact-right">
         <p>* Povinné položky</p>
-        <input type="checkbox" name="personal-data-checkbox" id="personal-data-checkbox">Súhlasím so spracovaním osobných údajov</br>
-        <button type="stubmit" id="contact-form-submit-btn" class="btn form-btn btn-default">Submit</button>
+        <p>
+            <input type="checkbox" name="personal-data-checkbox" id="personal-data-checkbox"> Súhlasím so spracovaním osobných údajov v zmysle GDPR. *</br>
+
+        </p>
+    </div>
+        <button type="submit" id="contact-form-submit-btn" title="Pre odoslanie vyplň povinné položky" class="btn form-btn btn-default">odoslať správu</button>
     
     
     <small class="text-info form-control-msg js-form-submission">Prebieha odosielanie, prosím čakajte..</small>
