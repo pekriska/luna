260f6bb6baa2619d02363d1dbca95318b0c10ff8
Peter Nagy <42371686+NagyDRCS@users.noreply.github.com>
2018-12-28 14:47:20 +0100

    2.0.2

--- inc/templates/contact-message-html.php
+++ inc/templates/contact-message-html.php
@@ -21,7 +21,14 @@
                     text-indent: 20px;
                     text-align: justify;
                 }
-
+                    .text-content{
+                        background: #FAFAFA;
+                        border-radius: 20px;
+                        padding: 20px;
+                    }
+                header img{
+                    width: 30%;
+                }
 
                 .footer-sub-text{
                     font-size: .8em;
@@ -47,14 +54,14 @@
         <body>
                 <header>
                     <div class="wrapp">
-                    <img src="<?php echo $image[0]; ?>">
+                        <img src="<?php echo $image[0]; ?>">
                     </div>
                 
                 </header>
                 <main class="wrapp">
-                    <div>
+                    <div class="">
                         <p>Ahoj Lenka, <b><?php echo $title;?></b> ti zasiela nasledovnú správu</p>
-                        <p class="text"><i><?php echo $message; ?></i></p>
+                        <p class="text text-content"><i><?php echo $message; ?></i></p>
                     </div>
                 </main>
                 <footer>
@@ -63,30 +70,9 @@
                         <p class="center sig">DROSSEL | CREATIVE STUDIO</p>
                     </div>
                 </footer>
-
-
-
-
-            <!-- Top title with dark background -->
-            <table style="background-color: #444; width: 100%;" cellspacing="0" cellpadding="0">
-                <tr>
-                    <td style="padding: 20px; text-align: center; font-family: sans-serif; color: #fff; font-size: 28px">
-                        <!-- <?php echo get_bloginfo('name'); ?> -->
-                    </td>
-                </tr>
-            </table>
     
             <!-- Main table 100% wide with background color #eee -->    
-            <table style="background-color: #eee; width: 100%;">
-                <tr>
-                    <td align="center" style="padding: 15px;">
-    
-                        <!-- Content table with backgdound color #fff, width 500px -->
-                        <table style="background-color: #fff; max-width: 600px; width: 100%; border: 1px solid #ddd;">
-                            <tr>
-                                <td style="padding: 15px; color: #333; font-size: 16px; font-family: sans-serif">
-                                    <!-- The <p>This message confirms your subscription to our newsletter. Thank you!</p><hr><p><a href="http://localhost/wordpress/?na=profile&nk=2-ef505df59e">Change your profile</p> tag is replaced with one of confirmation, welcome or goodbye messages -->
-                                    <!-- Messages content can be configured on Newsletter List Building panels --> 
+           
                                     <p><?php 
                                             echo $title;
                                             if($phone != ''){
@@ -94,24 +80,4 @@
                                             }
                                         ?>
                                     </p>
-                                    <p><?php echo $message; ?></p>
-                                    <hr>
-                                    <!-- Signature if not already added to single messages (surround with <p>) -->
-                                    <p>
-                                        <small>
-                                            <a href="<?php echo home_url(); ?>"><?php echo home_url(); ?></a><br>
-                                            <br>
-                                            
-                                        </small>
-                                    </p>
-                                
-                                </td>
-                            </tr>
-                        </table>
-    
-                    </td>
-                </tr>
-            </table>
-    
-        </body>
-    </html>
+                                    
