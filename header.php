bb6ce5054aa5bed9bd88b3695ddab76c4d5ad6c7
Peter Nagy <42371686+NagyDRCS@users.noreply.github.com>
2019-02-16 11:56:08 +0100

    2.0.3

--- header.php
+++ header.php
@@ -26,22 +26,27 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding wrapp">
+			<div class="site-logo">
 			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$luna_description = get_bloginfo( 'description', 'display' );
-			if ( $luna_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $luna_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
+				the_custom_logo();
+					if ( is_front_page() && is_home() ) :
+						?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<?php
+					else :
+						?>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+						<?php
+					endif;
+					$luna_description = get_bloginfo( 'description', 'display' );
+					if ( $luna_description || is_customize_preview() ) :
+						?>
+						<p class="site-description"><?php echo $luna_description; /* WPCS: xss ok. */ ?></p>
+				<?php endif; ?>
+			</div>
+			<div class="burger">
+			X
+			</div>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
--- page.php
+++ page.php
@@ -16,7 +16,7 @@ get_header();
 ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main wrapp">
+		<main id="main" class="site-main">
 
 		<?php
 		while ( have_posts() ) :
