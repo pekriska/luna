2b8e23b774045c844500f2095dc486d72164bec4
Peter Nagy <42371686+NagyDRCS@users.noreply.github.com>
2019-02-17 13:48:32 +0100

    2.0.6.10
    burger bug - solved

--- js/navigation.js
+++ js/navigation.js
@@ -1,25 +1,41 @@
-/**
- * File navigation.js.
- *
- * Handles toggling the navigation menu for small screens and enables TAB key
- * navigation support for dropdown menus.
- */
 $ = jQuery.noConflict(true);
 
 $(document).ready( function () {
-$("#primary-menu").css("display","none");
 
-$('.menu-toggle').click(function(e) {
-   e.preventDefault();
-    if ($('#primary-menu').is(":visible") ) {
-      $('.menu-toggle').toggleClass("is-active");
-      $('#primary-menu').stop(true,true).slideUp(700);
+var breakpoint = 860;
+
+if ($(document).width() <= breakpoint){
+  $("#primary-menu").css("display","none");
+
+}
+
+  $('.menu-toggle').click(function(e) {
+    e.preventDefault();
+
+      if ($('#primary-menu').is(":visible") ) {
+        $('.menu-toggle').removeClass("is-active");
+        $('#primary-menu').stop(true,true).slideUp(400);
+        
+      } else {
+        $('.menu-toggle').addClass("is-active");
+        $('#primary-menu').stop(true,true).slideDown(400); 
+      }
       
-    } else {
-      $('.menu-toggle').toggleClass("is-active");
-      $('#primary-menu').stop(true,true).slideDown(700);
-      
+    });
+
+
+  sizeOfScreen = $(window).width();
+
+  $(window).resize(function () {
+    
+    if ($(document).width() > breakpoint){
+      $("#primary-menu").css("display","block");
+		}else{
+      $("#primary-menu").css("display","none");
+      $('.menu-toggle').removeClass("is-active");
     }
+
+		sizeOfScreen = $(window).width();
   });
 
 });
